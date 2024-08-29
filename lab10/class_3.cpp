6f4846ac016b3bce3a4bc96718929b9ab08e81bc
Madhav-Paudel <studywith122@gmail.com>
2024-08-29 07:06:29 +0545

    Add files via upload

---
+++ lab10/class_3.cpp
@@ -0,0 +1,23 @@
+// WAP to illustrate simple example using try, catch and throw of exception handling.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int neo,deo,result;
+    cout<<"enter neo and deo"<<endl;
+    cin>>neo>>deo;
+    try
+    {
+        if(deo==0)
+        {
+            throw deo;
+        }
+        result=neo/deo;
+    }
+    catch(int ex)
+    {
+       cout<<"this is not possible "<<ex<<endl; 
+    }
+    cout<<"division is :"<<result<<endl;
+    
+}
